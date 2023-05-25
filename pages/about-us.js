bb39c8c9b2e3c18c31fb6fd0ee7e77d5d7ceb10f
Stan-wilbrord Ikalumhe <stanwilbrord@gmail.com>
2023-05-25 11:59:41 +0100

    about-us page

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,42 +1,38 @@
-import JobCard from "@/components/JobCard";
-import { jobsData } from "@/data/Job-data";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import JobCard from "@/components/JobCard"; 
+import { jobsData } from "@/data/jobsdata";
 
-export default function About_us (){
+export default function AboutUs () {
+    const router = useRouter();
+    
+    // setTimeout(() => {
+    //     return router.push('/search')
+    // },3000)
 
-    // const router = useRouter();
+    //console.log(router.query.keyword);
 
-    setTimeout(() => {
-        return router.push('/search')
-    },3000)
-
-    console.log(router.query);
-
-    useEffect (() => {
+    //run something after page has been render
+    useEffect(() => {
         const rNums = [];
 
-        for(let count = 0;count < 100;count ++){
-            rNums.push(Math.round(Math.random() * 100000))
+        for(let count = 0;count < 100;count++){
+            rNums.push(Math.round(Math.random() * 100000));
         }
-
     },[]);
+
     return (
-        <div> 
-            {jobsData.map((job) => {
-               return <JobCard key={job.id} title={job.title} description={job.description}>
-                    <h1>Info block</h1>
-                    <p>some information</p>
-                </JobCard>
-            })}
-
-
-
-
-
-            {/* <JobCard title='remote react next js developer'  description='jobs'/>
-            <JobCard title='Front end developer'  description='web developer'/> */}
-        </div>
+       <div>
+            {
+                jobsData.map(job => {
+                    return (
+                        <JobCard title={job.title} location={job.loc} key={job.id}>
+                            <h1>Info block</h1>
+                            <p>Some information</p>
+                        </JobCard>
+                    )
+                })
+            }
+       </div>
     )
 }
-
